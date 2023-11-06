968e96bb4649e632cd0e928a9859057a6d5ca4d6
professorfernanda <fmurillo@sdgku.edu>
2023-11-06 11:17:26 -0800

    Contact section

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False  
+EMAIL_HOST_USER = 'mariafeernanda.murillo@gmail.com'  
+EMAIL_HOST_PASSWORD = 'atdc wusr dxhz qlrj'
 
 # Application definition
 
--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 # Create your views here.
 def home(request):
     return render(request, 'pages/about_me.html')
 
 def contact(request):
-    form = ContactForm() #Create an instance of the form
 
-    
+    if request.method == 'POST':
+        form = ContactForm(request.POST) 
+        
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email_from = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            html = render_to_string("pages/email.html", request.POST)
+
+            send_mail("Message from " + name, message, email_from, ['mariafeernanda.murillo@gmail.com'], html_message=html)
+            
+            print("Enviado");
+            return redirect('home')
+        else:
+            print("No se pudo enviar el mensage");
+        
+    else:
+        form = ContactForm()
 
     return render(request, 'pages/contact.html', {
         'form': form
