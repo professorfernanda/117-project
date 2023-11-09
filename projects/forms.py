0574b657681495e27e4f668d8c6b31586ca6f5dd
professorfernanda <fmurillo@sdgku.edu>
2023-11-08 20:12:25 -0800

    Projects model

---
+++ projects/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Project
+
+class ProjectForm(forms.ModelForm):
+    class Meta: 
+        model = Project
+        fields = ['name', 'description', 'year', 'img', 'repository', 'technologies']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Projects Name'}),
+
+            'description': forms.Textarea(attrs={'placeholder': 'Projects Description'}),
+
+            'year': forms.NumberInput(attrs={'placeholder': 'Year'}),
+
+            'img': forms.ClearableFileInput(attrs={'placeholder': 'Select an Image'}),
+
+            'repository': forms.URLInput(attrs={'placeholder': 'Repositiry URL'}), 
+        }
---
+++ projects/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from .forms import ProjectForm
+from .models import Project
+
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/projects_list.html', {'projects': projects})
+
+def project_new(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save()
+            return redirect('projects_list')
+    else:
+        form = ProjectForm()
+
+    return render(request, 'projects/project_new.html', {
+        'form': form
+    })
